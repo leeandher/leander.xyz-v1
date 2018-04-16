a382a2d28e11c0d12b89ab561b92df00f20deab8
LeeAndHer <leandergrodrigues@gmail.com>
2018-04-15 20:04:54 -0400

    removed confidential information

--- sendForm.php
+++ sendForm.php
@@ -1,5 +1,4 @@
 <?php
-  if ( isset( $_POST['sender'] ) ) {
     $to = 'me@leander.xyz';
     $from = 'no-reply@leander.xyz';
     $subject = $_POST['subject'];
@@ -11,39 +10,89 @@
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\Exception;
     $path = $_SERVER['DOCUMENT_ROOT'];
-    require $path.'/inc/PHPMailer/Exception.php';
-    require $path. '/inc/PHPMailer/PHPMailer.php';
-    require $path. '/inc/PHPMailer/SMTP.php';
+    require $path . '/inc/PHPMailer/Exception.php';
+    require $path . '/inc/PHPMailer/PHPMailer.php';
+    require $path . '/inc/PHPMailer/SMTP.php';
+    require '../etc/mail-config.php';
 
-    $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+  if ( isset( $_POST['sender'] ) ) {
+    $mail = new PHPMailer(true);
     try {
         //Server settings
-        //$mail->SMTPDebug = 2;                                 // Enable verbose debug output
-        $mail->isSMTP();                                      // Set mailer to use SMTP
-        $mail->Host = 'leander.xyz';  // Specify main and backup SMTP servers
-        $mail->SMTPAuth = true;                               // Enable SMTP authentication
-        $mail->Username = 'no-reply@leander.xyz';                 // SMTP username
-        $mail->Password = 'AutoBotEmail3187';                           // SMTP password
-        $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-        $mail->Port = 465;                                    // TCP port to connect to
-
+        $mail->isSMTP();
+        $mail->Host = __MAILHOST__;
+        $mail->SMTPAuth = true;
+        $mail->Username = __MAILER__;
+        $mail->Password = __MAILPASS__;
+        $mail->SMTPSecure = __MAILSECURE__;
+        $mail->Port = __MAILPORT__;
+      
         //Recipients
         $mail->setFrom($from, 'Autobot');
-        $mail->addAddress($to, 'Leander Rodrigues');            // Name is optional
-        $mail->addReplyTo('info@example.com', 'Information');
+        $mail->addAddress($to, 'Leander Rodrigues');
 
         //Content
-        $mail->isHTML(true);                                  // Set email format to HTML
+        $mail->isHTML(true);
         $mail->Subject = $subject;
         $mail->Body    = $message;
         $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
         $mail->send();
-        echo 'Message has been sent';
+        //echo 'Message has been sent';
     } catch (Exception $e) {
-        echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+        //echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
     }
   }
-  header("Location: http://leander.xyz/success.php");
-  die();
 ?>
+
+
+<!DOCTYPE html>
+<html>
+
+  <?php
+    $pageTitle = "Success!";
+    $pageName = "redirect";
+    $path = $_SERVER['DOCUMENT_ROOT'];
+    $path .= "/inc/header.php";
+    require_once($path);
+  ?>  
+
+
+  <body>
+    <!--NAVIGATION-->
+    <?php 
+      $path = $_SERVER['DOCUMENT_ROOT'];
+      $path .= "/inc/navbar.html";
+      require_once($path);
+    ?>
+
+    <!--HOME SECTION-->
+    <div class="section dark" id="home">
+      <div class="content">
+        <div class="center" id="title">
+          Hi, my name is<br />
+          <span class="emphasis" id="name">Leander Rodrigues</span>.
+          <div id="subtitle">
+            I'm an undergrad
+            <span class="emphasis">engineer</span>, and aspiring
+            <span class="emphasis">programmer</span>.
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <?php
+      
+    ?>
+
+
+    <!--FOOTER SECTION-->
+    <?php 
+      $path = $_SERVER['DOCUMENT_ROOT'];
+      $path .= "/inc/footer.php";
+      require_once($path);
+    ?>
+
+  </body>
+
+</html>
