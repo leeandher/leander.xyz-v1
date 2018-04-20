0b3479005f9c47ec0631fea33b654b8d27163236
LeeAndHer <leandergrodrigues@gmail.com>
2018-04-20 19:28:14 -0400

    Article class, get requests, and deleted post directory

--- blog/index.php
+++ blog/index.php
@@ -1,5 +1,6 @@
 <?php
-  require '../../etc/classes/DB.php';
+  require_once '../../etc/classes/DB.php';
+  require_once '../../etc/classes/Article.php';
 ?>
 <!DOCTYPE html>
 <html>
@@ -10,8 +11,8 @@
     $path = $_SERVER['DOCUMENT_ROOT'];
     $path .= "/inc/header.php";
     require_once($path);
+    echo '<link rel="stylesheet" type="text/css" href="/resources/css/post.processed.css">';
   ?>
-
 
   <body>
     <!--NAVIGATION-->
@@ -21,6 +22,44 @@
       require_once($path);
     ?>
 
+    <?php
+      if (isset($_GET['viewArticle'])) {
+          $title = $_GET['viewArticle'];
+          $article_data = DB::getArticleData($title);
+          if (!$article_data) header('Location: .');
+          $blog_post = new Article($article_data);
+    ?>
+    <!--TITLE SECTION-->
+    <div class="section dark" id="post-head">
+      <div class="content">
+        <div id="post-quote"><?php echo $article_data['quote']; ?></div>
+      </div>
+    </div>
+
+    <!--RETURN SECTION-->
+    <a class="text-small light" id="page-return" href="."><i class="fas fa-arrow-left"></i> Back to Archive</a>
+
+    <!--POST SECTION-->
+    <div class="section" id="post-content">
+      <div class="content light">
+        <div id="post-share">
+          <a class="fab fa-facebook-f" href="#"></a>
+          <a class="fab fa-twitter" href=""></a>
+          <a class="fab fa-google-plus-g" href="#"></a>
+          <a class="fab fa-linkedin-in" href="#"></a>
+          <a class="fas fa-link"></a>
+        </div>
+        <?php
+
+          $blog_post->viewArticle ();
+
+        ?>
+      </div>
+    </div>
+
+    <?php
+      } else {
+    ?>
     <!--TITLE SECTION-->
     <div class="section dark" id="head">
       <div class="content">
@@ -43,79 +82,30 @@
       <div class="content">
         <div class="flex-group">
           <?php
-            $con = DB::getConnection();
-            $sql = 'SELECT * FROM Blogs ORDER BY Date DESC';
-            foreach ($con->query($sql) as $key => $article) {
-                echo '<a class="media light round" id="article-'.$key,'" href="post/index.html">';
-                echo '<div class="title">'.$article['Title'].'</div>';
-                echo '<div class="date text">'.$article['Date'].'</div>';
-                echo '<div class="summary text-small">'.strip_tags(substr($article['Markup'],0,350)).'...</div><div class="tags text-small">';
-                foreach (explode(' ',$article['Tags']) as $tag) {
-                  echo '<span class="round">'.$tag.'</span>';
-                }
-                echo '</div></a>';
+
+            $blog_data = DB::getAllBlogData();
+
+            foreach ($blog_data as $article) {
+              $post = new Article($article);
+              $post->preview ();
             }
-          ?><!--
-          <a class="media light round" id="article-1" href="post/index.html">
-            <div class="title">1 Lorem ipsum dolor sit amet</div>
-            <div class="date text">Lorem ipsum</div>
-            <div class="summary text-small">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel fermentum lectus, molestie finibus orci. Integer tristique nunc augue, pharetra interdum nunc mollis rhoncus. Nunc sed eleifend quam, non pharetra ante. Curabitur nec tincidunt arcu. In sit amet turpis sit amet augue consectetur scelerisque. Vivamus convallis sapien sed mauris placerat, at tincidunt odio pellentesque. Vestibulum eleifend sit amet felis ut semper. Vestibulum commodo pulvinar molestie. Mauris tempus convallis quam id faucibus. Duis a porttitor nibh.</div>
-            <div class="tags text-small">
-              <span class="round">review</span>
-              <span class="round">games</span>
-              <span class="round">coding</span>
-            </div>
-          </a>
-          <a class="media light round" id="article-2" href="#">
-            <div class="title">2 Lorem ipsum dolor sit amet</div>
-            <div class="date text">Lorem ipsum</div>
-            <div class="summary text-small">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel fermentum lectus, molestie finibus orci. Integer tristique nunc augue, pharetra interdum nunc mollis rhoncus. Nunc sed eleifend quam, non pharetra ante. Curabitur nec tincidunt arcu. In sit amet turpis sit amet augue consectetur scelerisque. Vivamus convallis sapien sed mauris placerat, at tincidunt odio pellentesque. Vestibulum eleifend sit amet felis ut semper. Vestibulum commodo pulvinar molestie. Mauris tempus convallis quam id faucibus. Duis a porttitor nibh.</div>
-            <div class="tags text-small">
-              <span class="round">review</span>
-              <span class="round">personal</span>
-              <span class="round">tv/movies</span>
-            </div>
-          </a>
-          <a class="media light round" id="article-3" href="#">
-            <div class="title">3 Lorem ipsum dolor sit amet</div>
-            <div class="date text">Lorem ipsum</div>
-            <div class="summary text-small">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel fermentum lectus, molestie finibus orci. Integer tristique nunc augue, pharetra interdum nunc mollis rhoncus. Nunc sed eleifend quam, non pharetra ante. Curabitur nec tincidunt arcu. In sit amet turpis sit amet augue consectetur scelerisque. Vivamus convallis sapien sed mauris placerat, at tincidunt odio pellentesque. Vestibulum eleifend sit amet felis ut semper. Vestibulum commodo pulvinar molestie. Mauris tempus convallis quam id faucibus. Duis a porttitor nibh.</div>
-            <div class="tags text-small">
-              <span class="round">games</span>
-              <span class="round">announcement</span>
-              <span class="round">coding</span>
-            </div>
-          </a>
-          <a class="media light round" id="article-4" href="#">
-            <div class="title">4 Lorem ipsum dolor sit amet</div>
-            <div class="date text">Lorem ipsum</div>
-            <div class="summary text-small">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel fermentum lectus, molestie finibus orci. Integer tristique nunc augue, pharetra interdum nunc mollis rhoncus. Nunc sed eleifend quam, non pharetra ante. Curabitur nec tincidunt arcu. In sit amet turpis sit amet augue consectetur scelerisque. Vivamus convallis sapien sed mauris placerat, at tincidunt odio pellentesque. Vestibulum eleifend sit amet felis ut semper. Vestibulum commodo pulvinar molestie. Mauris tempus convallis quam id faucibus. Duis a porttitor nibh.</div>
-            <div class="tags text-small">
-              <span class="round">coding</span>
-              <span class="round">tv/movies</span>
-              <span class="round">project</span>
-            </div>
-          </a>
-          <a class="media light round" id="article-5" href="#">
-            <div class="title">5 Lorem ipsum dolor sit amet</div>
-            <div class="date text">Lorem ipsum</div>
-            <div class="summary text-small">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel fermentum lectus, molestie finibus orci. Integer tristique nunc augue, pharetra interdum nunc mollis rhoncus. Nunc sed eleifend quam, non pharetra ante. Curabitur nec tincidunt arcu. In sit amet turpis sit amet augue consectetur scelerisque. Vivamus convallis sapien sed mauris placerat, at tincidunt odio pellentesque. Vestibulum eleifend sit amet felis ut semper. Vestibulum commodo pulvinar molestie. Mauris tempus convallis quam id faucibus. Duis a porttitor nibh.</div>
-            <div class="tags text-small">
-              <span class="round">games</span>
-              <span class="round">personal</span>
-            </div>
-          </a>-->
+
+          ?>
         </div>
         <div id="error">No results, sorry!</div>
       </div>
     </div>
 
+    <?php
+      }
+    ?>
     <!--FOOTER SECTION-->
     <?php
       $path = $_SERVER['DOCUMENT_ROOT'];
       $path .= "/inc/footer.php";
       require_once($path);
     ?>
+    <script src="/resources/js/post.js"></script>
 
   </body>
 
