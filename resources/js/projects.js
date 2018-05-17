185b344203544b7ceb5fa1db87cc3cd7355199f9
LeeAndHer <leandergrodrigues@gmail.com>
2018-05-17 11:13:38 -0400

    Changed all project descriptions to add a more 'me'-like feel, as well as some modifications to the 'intention'

--- resources/js/projects.js
+++ resources/js/projects.js
@@ -21,7 +21,7 @@ function applyProject(proj, order) {
   $(projId + ' .live').prop('href','/projects/' +  proj.link + '/index.html');
   $(projId + ' .source').prop('href', 'https://github.com/leeandher/leeandher.github.io/tree/master/projects/' + proj.link);
   $(projId + ' .proj-date').text(proj.date);
-  $(projId + ' .proj-desc').text(proj.desc);
+  $(projId + ' .proj-desc').html(proj.desc);
   $(projId + ' .languages').text(arrToList(proj.languages));
   $(projId + ' .additional').text(arrToList(proj.additional));
   $(projId + ' .concepts').text(arrToList(proj.concepts));
@@ -33,34 +33,34 @@ var projectArr = [
   new Project(
     'Random Quote Machine',
     'February 04, 2018',
-    "A webpage styled as a mobile phone with the Tinder app open that cycles through different lame pick-up lines, names, locations, and background images.",
+    "Since I'm too awkward to use the real Tinder, I made this re-creation to test out my basic front-end skills. It cycles through lame pick-up lines, silly names, weird locations and some background images to create replica Tinder profiles. You can even see all your matches by checking out the LikeList (the heart in the top left).",
     ['HTML5', 'CSS3', 'JavaScript'],
     ['Bootstrap 4', 'SCSS', 'JQuery'],
-    ['Design Recreation'],
+    ['Design Re-creation'],
     'quotes'
   ),
   new Project(
     'Wikipedia Viewer',
     'February 07, 2018',
-    "A front-end face that accesses the Wikipedia API to search for results and display in a clean format. Accepts and input and displays the top 20 results.",
+    "Even though Wikipedia might be humanities greatest collaborations (excluding Space Jam (1996)), I still think the website itself could use some sprucing up. This viewer app accesses the Wikipedia Query API to display upto 20 results for a cleaner, refined viewing experience. Click on any of your search results for even more information.",
     ['HTML5', 'CSS3', 'JavaScript'],
     ['JSON APIs', 'Bootstrap 4', 'Haml', 'SCSS', 'JQuery', 'Popper.js'],
-    ['String Manipulation', 'Live Response'],
+    ['String Manipulation', 'Live API Call/Response'],
     'wikiview'
   ),
   new Project(
     'Local Weather App',
     'February 10, 2018',
-    "A basic weather app which accesses the OpenWeatherMap API through FCC's glitch.me using the geolocation in the user's browser and displays statistics and information about the weather in their location.",
+    "Let's be honest, the outdoors is a scary, unforgiving place. If you're like me, you want to know what it's like out there without having to risk a lightning strike, heat stroke, or social interaction. That's why I build this Weather App, which calls upon the OpenWeatherMap API to tell you about that horrible place we call 'outside'. <br /><br /> <b>NOTE:</b> The API is routed through freeCodeCamp.org to avoid Authentication, and can sometimes return incorrect data. A couple 'refreshes' usually does the trick.",
     ['HTML5', 'CSS3', 'JavaScript'],
-    ['Bootstrap 4', 'Haml', 'SCSS', 'JQuery'],
-    ['JSON APIs', 'HTML5 Geolocation', 'Open Source APIs'],
+    ['JSON APIs', 'Bootstrap 4', 'Haml', 'SCSS', 'JQuery'],
+    ['HTML5 Geolocation', 'Data Management'],
     'weather'
   ),
   new Project(
     'Twitch.tv Stream Client',
     'February 15, 2018',
-    "A webpage with an array of Twitch.tv streamers which accesses the Twitch.tv API through FCC's glitch.me and displays information about the streamer's current status and profile. There are also filters/sorts which dynamically affect the data shown.",
+    "Videogame Livestreaming is the new hotness. If you want to stay up-to-date with all your favourite streamers, this Twitch.tv client I developed is going to be your best friend. It grabs a bunch of useful information about the streamer's current status (from the Twitch.tv API) and puts it all in one convenient dashboard. Add, sort, and filter dynamically to your heart's content. GLHF!",
     ['HTML5', 'CSS3', 'JavaScript'],
     ['Bootstrap 4', 'Haml', 'SCSS', 'JQuery'],
     ['JSON APIs', 'AJAX', 'Commercial APIs', 'Live Response'],
@@ -69,7 +69,7 @@ var projectArr = [
   new Project(
     'JavaScript Calculator',
     'March 06, 2018',
-    "A calculator app that runs entirely on complex hand-coded JavaScript. It is meant to be a desktop calculator which ignores BEDMAS so performs operations linearly. It scales responsively for mobile and does not implore the 300ms delay, allowing for faster inputs.",
+    "This web app is the one-stop shop for quick maths. This full-page calculator app runs entirely on my hand-coded JavaScript. It performs linear operations (no PEDMAS) hyper fast and supports numpad/keystroke input as well. It's even responsive on mobile, so you can finally ditch that pocket calculator you lug around everywhere. Why did you have that in the first place?",
     ['HTML5', 'CSS3', 'JavaScript'],
     ['Bootstrap 4', 'Haml', 'SCSS', 'JQuery'],
     ['Basic Mathematics'],
@@ -78,7 +78,7 @@ var projectArr = [
   new Project(
     'Pomodoro Clock',
     'March 12, 2018',
-    "A timer using the pomodoro productivity technique, of set break and work timers to regiment time spent allocated to tasks. It is styled as a fuel gauge which empties and refills on work periods and break periods respectively.",
+    "If you have trouble staying productive, one of the most common recommendations is to try the Pomodoro Technique. You specify break, and work timers to regulate the time you spend on <i>Facebook</i> against the time you spend with your <i>face in the books</i>. Decide your own times, and click start to see a stylized productivity fuel gauge with depletes and refills endlessly. Finally a way to write that book you keep talking about.",
     ['HTML5', 'CSS3', 'JavaScript'],
     ['Bootstrap 4', 'Haml', 'SCSS', 'JQuery'],
     ['JS Timing Functions'],
@@ -87,16 +87,16 @@ var projectArr = [
   new Project(
     'Simon Game',
     'March 15, 2018',
-    "A recreation of the famous toy which calls on the user's memory of a sequence of button presses in order to win. A few additonal features have been added, such as strict-mode, spin-mode, no-repeat mode and multiple colour schemes.",
+    "Back in 1978, Milton Bradley distributed this electronic memory game to distract children and give their parents some breathing room. This faithful re-creation of said game, has all the fun features of the original plus my own <i>spin</i> on things (The joke is that one of my features is spin-mode). Turning on your sound helps quite a bit, and if you manage to get to 20, Congratulations! Send me a link to a screenshot and I'll comment your name into the source code.",
     ['HTML5', 'CSS3', 'JavaScript'],
     ['Bootstrap 4', 'Haml', 'SCSS', 'JQuery'],
-    ['Web Audio API'],
+    ['Array Manipulation','Web Audio API'],
     'simon'
   ),
   new Project(
     'Tic-Tac-Toe',
     'March 21, 2018',
-    "A implementation of Tic-Tac-Toe with multiple settings. It allows for Player vs. Player (with names, colours, and shape selection) and Player vs. AI (with everything else, and AI difficulty settings). One of the difficulty settings is 'Impossible' which uses the minimax algorithm for an unbeatable Tic-Tac-Toe AI.",
+    "It's time to grab a pal and showdown in the greatest strategy game of 1300 BCE. If you don't have a partner, I've got you covered: AI. This implementation of Tic-Tac-Toe (Naughts and Crosses, if you prefer) allows for both Player vs. Player and Player vs. AI. My AI even has difficulty settings, for you to hone your TTT Skills, however, avoid the 'Impossible' difficulty, unless you wan't TacTic Bot to show you how Terminator should have ended.",
     ['HTML5', 'CSS3', 'JavaScript'],
     ['Bootstrap 4', 'Haml', 'SCSS', 'JQuery'],
     ['AI Algorithmic Logic'],
