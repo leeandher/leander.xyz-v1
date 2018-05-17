07b0c9b39e6e659d18412374601d6c30a3b510e6
LeeAndHer <leandergrodrigues@gmail.com>
2018-05-17 14:09:59 -0400

    typo in resume fixed, and JS calculator description edited

--- resources/js/projects.js
+++ resources/js/projects.js
@@ -72,7 +72,7 @@ var projectArr = [
     "This web app is the one-stop shop for quick maths. This full-page calculator app runs entirely on my hand-coded JavaScript. It performs linear operations (no PEDMAS) hyper fast and supports numpad/keystroke input as well. It's even responsive on mobile, so you can finally ditch that pocket calculator you lug around everywhere. Why did you have that in the first place?",
     ['HTML5', 'CSS3', 'JavaScript'],
     ['Bootstrap 4', 'Haml', 'SCSS', 'JQuery'],
-    ['Basic Mathematics'],
+    ['Dynamic Mathematics'],
     'calculator'
   ),
   new Project(
